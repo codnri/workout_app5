fb7f2d49a3ae24ad3fa62ea950a0bba0dcd06fea
coddy <izo.kodak+ror@gmail.com>
2018-03-18 23:07:43 +0000

    Add user validations

--- spec/features/exercises/listing_exercises_spec.rb
+++ spec/features/exercises/listing_exercises_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.feature "Listing Exercises" do
   before do
-    @john =  User.create(email: "john@example.com", password: "password")
+    @john =  User.create(first_name: "John", last_name: "Doe", email: "john@example.com", password: "password")
     # @sarah = User.create(first_name: "Sarah", last_name: "Anderson", email: "sarah@example.com", password: "password")
     
     login_as(@john)
--- spec/features/users/signing_users_in_spec.rb
+++ spec/features/users/signing_users_in_spec.rb
@@ -1,28 +1,41 @@
 require "rails_helper"
- 
-RSpec.feature "Users signin" do
- 
-  before do
-   
-    @john = User.create!(email: "john@example.com", password: "password")
- 
-  end
- 
+
+RSpec.feature "Users signup" do
   scenario "with valid credentials" do
-   
     visit "/"
-   
-    click_link "Sign in"
-   
-    fill_in "Email", with: @john.email
-   
-    fill_in "Password",  with: @john.password
-   
-    click_button "Log in"
-   
-    expect(page).to have_content("Signed in successfully.")
-   
-    expect(page).to have_content("Signed in as #{@john.email}")
- 
+
+    click_link "Sign up"
+    fill_in "First name", with: "John"
+    fill_in "Last name", with: "Doe"
+    fill_in "Email", with: "john@example.com"
+    fill_in "Password",  with: "password"
+    fill_in "Password confirmation",  with: "password"
+    click_button "Sign up"
+
+    expect(page).to have_content("You have signed up successfully.")
+    
+    user = User.last
+    # room = user.room
+    room_name = user.full_name.split.join('-')
+    # expect(room.name).to eq(room_name)
+    
+    visit "/"
+    expect(page).to have_content("John Doe")
   end
+  
+  scenario "with invalid credentials" do
+    visit "/"
+
+    click_link "Sign up"
+    fill_in "First name", with: ""
+    fill_in "Last name", with: ""
+    fill_in "Email", with: "john@example.com"
+    fill_in "Password",  with: "password"
+    fill_in "Password confirmation",  with: "password"
+    click_button "Sign up"
+
+    expect(page).to have_content("First name can't be blank")
+    expect(page).to have_content("Last name can't be blank")
+  end
+   
 end
